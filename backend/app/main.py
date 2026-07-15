e1c161a8a5d9112ac46db45a8a92f0f392fcd3d3
muu77muu <muumuu.codes@gmail.com>
2026-07-15 22:28:06 +0800

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,28 @@
 from dotenv import load_dotenv
 from pathlib import Path
+from os import getenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR / ".env.local")
 
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.routes.upload import router as upload_router
 from app.routes.documents import router as documents_router
 
 app = FastAPI()
+
+origins = [ getenv("FRONTEND_URL") ]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(upload_router)
 app.include_router(documents_router)
 
